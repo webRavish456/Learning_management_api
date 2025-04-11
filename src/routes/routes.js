8db77e7359fa176f2bb45f43ef26fb42c937575a
Manishakumari789 <manishakumari76983@gmail.com>
2025-04-11 14:09:37 +0530

    commit changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,8 @@ import { postAdmin } from '../controllers/authControllers.js';
 import { postForgot } from '../controllers/authControllers.js';
 import { deleteBranch, getBranch, getBranchById, postBranch, updateBranch } from '../controllers/branchControllers.js';
 import { deleteExam, getExam, getExamById, postExam, updateExam } from '../controllers/examControllers.js';
-
+import { deleteAllAssignment, getAllAssignment, getAllAssignmentById, postAllAssignment, updateAllAssignment } from '../controllers/allAssignmentControllers.js';
+import { deleteStudentsAssignment, getStudentsAssignment, getStudentsAssignmentById, postStudentsAssignment, updateStudentsAssignment } from '../controllers/studentsAssignmentControllers.js';
 
 
 export const router = express.Router();
@@ -22,3 +23,18 @@ router.route('/exam').get(getExam);
 router.route('/exam/:id').get(getExamById);
 router.route('/exam/:id').patch(updateExam);
 router.route('/exam/:id').delete(deleteExam);
+
+
+router.route('/allAssignment').post(postAllAssignment);
+router.route('/allAssignment').get(getAllAssignment);
+router.route('/allAssignment/:id').get(getAllAssignmentById);
+router.route('/allAssignment/:id').patch(updateAllAssignment);
+router.route('/allAssignment/:id').delete(deleteAllAssignment);
+
+
+
+router.route('/studentsAssignment').post(postStudentsAssignment);
+router.route('/studentsAssignment').get(getStudentsAssignment);
+router.route('/studentsAssignment/:id').get(getStudentsAssignmentById);
+router.route('/studentsAssignment/:id').patch(updateStudentsAssignment);
+router.route('/studentsAssignment/:id').delete(deleteStudentsAssignment);
